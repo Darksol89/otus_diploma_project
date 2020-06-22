b51501526910f308afa82d6230baf6e8ec2b9e31
Ivan_Chistov <Darksol89@gmail.com>
2020-06-22 18:48:56 +0300

    Change test

--- tests/test_add_new_issue.py
+++ tests/test_add_new_issue.py
@@ -1,19 +1,14 @@
 import allure
 import pytest
-from PageObject.DashboardPage import DashboardPage
 from PageObject.NavigationMenuPage import NavigationMenuPage
 from PageObject.IssuePage import IssuePage
 from PageObject.common.expected_results import ExpectedResults
 
 
 @allure.title('Add new issue testing')
-@pytest.mark.skip
-@pytest.mark.parametrize('dash_name, issue_name', [('Space-X', 'Space-X_issue')])
-def test_add_new_issue(browser_driver, get_url, login_to_youtrack, dash_name, issue_name):
+@pytest.mark.parametrize('issue_name', [('Space-X_issue')])
+def test_add_new_issue(browser_driver, get_url, login_to_youtrack, issue_name):
     """Test - Create new issue"""
-    DashboardPage(browser_driver) \
-        .click_dashboard_dropdown() \
-        .create_new_dashboard(dash_name)
     NavigationMenuPage(browser_driver).click_new_issue()
     IssuePage(browser_driver) \
         .add_summary_issue(issue_name) \
@@ -21,8 +16,3 @@ def test_add_new_issue(browser_driver, get_url, login_to_youtrack, dash_name, is
         .click_create_button()
 
     assert issue_name == ExpectedResults(browser_driver).check_issue_name()
-
-    NavigationMenuPage(browser_driver).click_dashboards()
-    DashboardPage(browser_driver) \
-        .click_edit_dashboard() \
-        .delete_dashboard(dash_name)
